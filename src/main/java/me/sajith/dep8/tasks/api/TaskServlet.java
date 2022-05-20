889b4631ee5dd7f998505be6d4b0bfaeead85211
sajith <sajithd784@gmail.com>
2022-05-20 17:21:35 +0530

    Add TaskServlet into the DispatcherServlet

--- src/main/java/me/sajith/dep8/tasks/api/TaskServlet.java
+++ src/main/java/me/sajith/dep8/tasks/api/TaskServlet.java
@@ -10,6 +10,10 @@ import java.io.IOException;
 @WebServlet(name = "TaskServlet")
 public class TaskServlet extends HttpServlet2 {
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request came here");
+    }
 
 
 }
