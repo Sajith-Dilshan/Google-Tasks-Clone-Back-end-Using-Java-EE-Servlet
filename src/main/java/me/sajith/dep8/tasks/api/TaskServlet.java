31be8acb2707296c29a787f38751916724e7ffff
sajith <sajithd784@gmail.com>
2022-05-20 17:10:25 +0530

    Add TaskServlet and Register the TaskServlet's Name

---
+++ src/main/java/me/sajith/dep8/tasks/api/TaskServlet.java
@@ -0,0 +1,15 @@
+package me.sajith.dep8.tasks.api;
+
+import me.sajith.dep8.tasks.util.HttpServlet2;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "TaskServlet")
+public class TaskServlet extends HttpServlet2 {
+
+
+
+}
