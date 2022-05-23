605193bea999a232bc4a9773962dab1a9c8d23f2
sajith <sajithd784@gmail.com>
2022-05-23 14:45:58 +0530

    fix error

--- src/main/java/me/sajith/dep8/tasks/api/DispatcherServlet.java
+++ src/main/java/me/sajith/dep8/tasks/api/DispatcherServlet.java
@@ -20,44 +20,37 @@ public class DispatcherServlet extends HttpServlet2 {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        if (req.getPathInfo() == null || req.getPathInfo().equals("/")){
-            if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+            getServletContext().getNamedDispatcher("UserServlet").forward(req, resp);
+        } else {
+            String pattern = "/([A-Fa-f0-9\\-]{36})/?.*";
+            Matcher matcher = Pattern.compile(pattern).matcher(req.getPathInfo());
+            if (matcher.find()) {
+                String userId = matcher.group(1);
+                if (!userId.equals(SecurityContextHolder.getPrincipal().getId())) {
+                    resp.setContentType("application/json");
+                    resp.setStatus(403);
+                    Jsonb jsonb = JsonbBuilder.create();
+                    jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 403, null,
+                            "Permission denied", req.getRequestURI()), resp.getWriter());
+                    return;
+                }
+            }
+
+            if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/?")) {
                 getServletContext().getNamedDispatcher("UserServlet").forward(req, resp);
-            }else{
-                if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/?")){
-                } else {
-                    String pattern = "/([A-Fa-f0-9\\-]{36})/?.*";
-                    Matcher matcher = Pattern.compile(pattern).matcher(req.getPathInfo());
-                    if (matcher.find()) {
-                        String userId = matcher.group(1);
-                        if (!userId.equals(SecurityContextHolder.getPrincipal().getId())) {
-                            resp.setContentType("application/json");
-                            resp.setStatus(403);
-                            Jsonb jsonb = JsonbBuilder.create();
-                            jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 403, null,
-                                    "Permission denied", req.getRequestURI()), resp.getWriter());
-                            return;
-                        }
-                    }
+            } else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists(/\\d+)?/?")) {
+                getServletContext().getNamedDispatcher("TaskListServlet").forward(req, resp);
+            } else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists/\\d+/tasks(/\\d+)?/?")) {
+                getServletContext().getNamedDispatcher("TaskServlet").forward(req, resp);
+            } else {
+                resp.setContentType("application/json");
+                resp.setStatus(404);
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 404, null,
+                        "Invalid location", req.getRequestURI()), resp.getWriter());
+            }
+        }
 
-                    if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/?")) {
-                        getServletContext().getNamedDispatcher("UserServlet").forward(req, resp);
-                    }else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists(/\\d+)?/?")) {
-                    } else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists(/\\d+)?/?")) {
-                        getServletContext().getNamedDispatcher("TaskListServlet").forward(req, resp);
-                    }else if(req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists/\\d+/tasks(/\\d+)?/?")){
-                    } else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists/\\d+/tasks(/\\d+)?/?")) {
-                        getServletContext().getNamedDispatcher("TaskServlet").forward(req, resp);
-                    }else{
-                        super.service(req,resp);
-                    } else {
-                        resp.setContentType("application/json");
-                        resp.setStatus(404);
-                        Jsonb jsonb = JsonbBuilder.create();
-                        jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 404, null,
-                                "Invalid location", req.getRequestURI()), resp.getWriter());
-                    }
-
+        }
     }
-
-}
